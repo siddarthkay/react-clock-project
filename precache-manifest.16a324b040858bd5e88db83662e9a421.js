9374080ff5285ba6d9a2e60bf1fc77f34a043a7d
siddarthkay <siddarthkay@gmail.com>
2020-05-10 15:12:36 +0530

    pushing clock component to github

---
+++ precache-manifest.16a324b040858bd5e88db83662e9a421.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "984b5b9c006a07d5348782a606001e9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77f6fa594dbf56e8c600",
+    "url": "/static/css/main.a000dcce.chunk.css"
+  },
+  {
+    "revision": "ef63d3237181a4076d8a",
+    "url": "/static/js/2.75d58725.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.75d58725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77f6fa594dbf56e8c600",
+    "url": "/static/js/main.95444329.chunk.js"
+  },
+  {
+    "revision": "54544d211bb412302c81",
+    "url": "/static/js/runtime-main.23afc54a.js"
+  }
+]);
